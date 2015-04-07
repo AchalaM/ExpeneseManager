46dd39732ab59bfa792921a7e5fd596180846d2b
AchalaM <madushan.achala.12@cse.mrt.ac.lk>
2015-04-07 11:05:02 +0530

    DBConnection added

---
+++ src/com/epicsoft/expensemanager/controller/DBController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.epicsoft.expensemanager.controller;
+
+/**
+ *
+ * @author hp
+ */
+public class DBController {
+    
+    public void addNewUser(){
+        
+    }
+    
+    public void createDB(){
+        
+    }
+    
+    public void createTables(){
+      
+    }
+    
+    public void dropDB(){
+        
+    }
+    
+}
---
+++ src/com/epicsoft/expensemanager/db/DBConnection.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.epicsoft.expensemanager.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author hp
+ */
+public class DBConnection {
+    
+    private static DBConnection dBConnection;
+    private Connection connection;
+    
+    private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+    private static final String DB_URL = System.getProperty("user.home", ".") + "\\ExpenseManager\\";
+    
+    /**
+     * @param JDBC_URL
+     * @param user
+     * @param password
+     * @throws ClassNotFoundException
+     * @throws SQLException 
+     */
+    private DBConnection(String JDBC_URL, String user, String password) throws ClassNotFoundException, SQLException{
+        Class.forName(DRIVER);
+        connection = DriverManager.getConnection(DB_URL + JDBC_URL, user, password);
+    }
+    
+    /**
+     * 
+     * @param JDBC_URL
+     * @param user
+     * @param password
+     * @return
+     * @throws ClassNotFoundException
+     * @throws SQLException 
+     */
+    public static DBConnection getInstance(String JDBC_URL, String user, String password) throws ClassNotFoundException, SQLException{
+        if (dBConnection == null) {
+            dBConnection = new DBConnection(JDBC_URL, user, password);
+        }
+        return dBConnection;
+    }
+    
+    /**
+     * 
+     * @param JDBC_URL
+     * @param user
+     * @param password
+     * @return 
+     */
+    public Connection getConnection(String JDBC_URL, String user, String password){
+        return connection;
+    }
+}
